fc84e32a10cb8d994f6dcae934e2e3fd5c95b723
aarzuthapa <aarju.thapa76@gmail.com>
2024-01-31 12:15:55 +0545

    Add login page and css page

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    
+    <form action="action_page.php" method="POST">
+    
+   
+    <label for="name">UserId</label>
+
+    <input type="text" name="id" placeholder="User Id"><br>
+
+    <label for="name">Password</label>
+
+    <input type="password" name="pw" placeholder="Password"><br> 
+
+    <button type="submit" name="submit">Login</button>
+
+</body>
+</html>
